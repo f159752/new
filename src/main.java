80a9f0e4defb3d778ad200bd61c58080d492ad0a
artem <artem@gmail.com>
2016-12-10 16:04:21 +0200

    1st commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by f1597 on 02.12.2016.
+ */
+public class main {
+    public static void main(String[] args){
+        my_class obj = new my_class(5,10);
+        System.out.print(obj.plus());
+    }
+}
---
+++ src/my_class.java
@@ -0,0 +1,22 @@
+/**
+ * Created by f1597 on 02.12.2016.
+ */
+public class my_class {
+    public int x;
+    public int y;
+
+    public my_class(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int plus(){
+        return x+y;
+    }
+    public int minus(){
+        return x-y;
+    }
+    public int umn(){
+        return x*y;
+    }
+}
